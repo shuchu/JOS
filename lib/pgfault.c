5c47cb6394f4c9aa2bb9a9297bb2527bc5455a2f
Shuchu <shhan@cs.stonybrook.edu>
2021-04-21 17:19:54 -0400

    lab4 finish user pgfault handlering.

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -29,7 +29,12 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
+		//panic("set_pgfault_handler not implemented");
+                int res = sys_page_alloc(0, (void*) (UXSTACKTOP - PGSIZE), PTE_U| PTE_P|PTE_W);
+                if (res < 0)
+                    panic("set pagfault_handler error: %e", res);
+                else
+                    sys_env_set_pgfault_upcall(0, _pgfault_handler);
 	}
 
 	// Save handler pointer for assembly to call.
