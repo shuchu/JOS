fefb9d36aa5aa5c8bf6e4baf1cb50d3a28dd9b2a
Shuchu <shhan@cs.stonybrook.edu>
2022-01-08 08:29:27 -0500

    finished exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,3 +2,16 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+
+volatile uint32_t *e1000;
+
+int pci_e1000_attach(struct pci_func *pcif)
+{
+    pci_func_enable(pcif);
+
+    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("e1000: bar0 %x size0 %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("e1000: status %x\n", e1000[E1000_STATUS/4]);
+
+    return 1;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,14 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+#define PCI_E1000_VENDOR 0x8086
+#define PCI_E1000_DEVICE 0x100E
+
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
+int pci_e1000_attach(struct pci_func *pcif);
+
+
 #endif  // SOL >= 6
